617f2c4accad62d3ac0825867ff83641c8fdb4af
x-stream <iljin.oleg.olegovich@gmail.com>
2015-09-15 15:05:03 +0600

    add svg

--- jquery.textstroke.js
+++ jquery.textstroke.js
@@ -2,19 +2,21 @@
  * Created by oleg on 15.09.2015.
  */
 
-/*global jQuery, document, console*/
+/*global jQuery, document, window, console*/
 (function ($) {
     "use strict";
 
-    $.fn.textstroke = function (options) {
+    $.fn.textStroke = function (options) {
 
         var settings = $.extend({
                 strokeColor : '#000',   // цвет линии
-                textColor   : true,     // цвет текста
+                textColor   : false,    // цвет текста
+                textSize    : false,    // цвет текста
                 size        : 1,        // размер линии (преобразуется к float)
                 steps       : 24,       // кол-во генерируемых слоев
                 limit       : 1e-6,     // минимальный порог для координат,
-                native      : false     // если true то для WebKit подобных браузеров будет использоваться webkitTextStroke
+                native      : false,    // если true то для WebKit подобных браузеров будет использоваться webkitTextStroke
+                svg         : true
             }, options),
 
             keyCache,
@@ -47,19 +49,48 @@
             };
 
         settings.size = parseFloat(settings.size);
+        settings.svg = settings.svg && (window.SVGSVGElement !== undefined);
         keyCache = settings.strokeColor + '_' + settings.size + '_' + settings.steps;
 
 
         return this.each(function () {
+            var $that = $(this),
+                svgRoot,
+                svgText;
 
             if (strokeSupport) {
-                $(this).css('-webkit-text-stroke', settings.size + 'px ' + settings.strokeColor);
+
+                $that.css('-webkit-text-stroke', settings.size + 'px ' + settings.strokeColor);
+
+            } else if (settings.svg) {
+
+                svgRoot = $('<svg><text></text></svg>').css('height', settings.textSize || $that.css('font-size'));
+                svgText = svgRoot.find('text');
+                svgText.text($that.text());
+
+                svgText.attr('x', 0);
+                svgText.attr('y', settings.textSize || $that.css('font-size'));
+                svgText.attr('class', $that.attr('class'));
+                svgText.attr('font-size', settings.textSize || $that.css('font-size'));
+                svgText.attr('font-weight', $that.css('font-weight'));
+                svgText.attr('font-family', $that.css('font-family'));
+                svgText.attr('stroke', settings.strokeColor);
+                svgText.attr('stroke-width', settings.size);
+                svgText.attr('fill', settings.textColor || 'none');
+
+                // fix IE
+                //svgRoot = '&nbsp;' + $('<div>').wrapInner(svgRoot).html() + '&nbsp;'
+
+                $that.html('').append(svgRoot);
+
             } else {
-                $(this).css('textShadow', getStroke(keyCache));
+
+                $that.css('textShadow', getStroke(keyCache));
+
             }
 
-            if (settings.textColor) {
-                $(this).css('color', settings.textColor);
+            if (settings.textColor && !settings.svg) {
+                $that.css('color', settings.textColor);
             }
 
         });
